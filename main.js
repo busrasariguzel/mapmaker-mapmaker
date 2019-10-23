7baa2ba4b109bc2b6c918648046a8f3041f49189
busrasariguzel <busra.sariguzel@codeimmersives.com>
2019-10-23 01:04:05 -0400

    done

--- main.js
+++ main.js
@@ -31,16 +31,18 @@ function yelledGreetings(arr) {
 function changeToInitials(arr) {
   const newArr = [];
   for(let i = 0; i < arr.length; i++){
-    const name = arr[i];
     const spaceIndex = arr[i].indexOf(' ');
     const lastInitialIndex = spaceIndex + 1;
     // let firstLetterOfLastName = arr[lastInitialIndex]
-    newArr.push(arr[i][0] + arr[lastInitialIndex].toUpperCase())
+    newArr.push(arr[i][0] + arr[i][lastInitialIndex])
   }
   return newArr
 }
 
-['Colin Jaffe', "Busra Sariguzel"]
+
+
+
+
 
 
 function doubleOdd(arr) {
@@ -61,24 +63,24 @@ function upperCaseFirstLetters(arr) {
   const newArr = [];
   for(let i = 0; i < arr.length; i++){
 
-    newArr.push(arr[i].slice(0,1).toUpperCase() + arr[i].slice(1).toUpperCase())
-
-    // newArr.push(arr[i].slice(-1,-2).toUpperCase())
+    newArr.push(arr[i].slice(0,1).toUpperCase() + arr[i].slice(1).toLowerCase())
   }
   return newArr;
 }
 
 
 function add1ToLeft(arr) {
-//   const newArr = [];
-//   for(let i = 0; i < arr.length; i++){
-//     if(arr[i]>0){
-//       newArr.push(arr[i])
-//     }
-
-// return newArr;
-// }
+  const newArr = [];
+  for(let i = 0; i < arr.length; i++){
+    if (arr[i] > 0){
+      newArr.push(Number('1' + arr[i]))
+    } else if (arr[i] < 0){
+      newArr.push(Number('-1' + arr[i]*-1))
+    }
+  }
+return newArr
 }
+
 
 
 module.exports = {
